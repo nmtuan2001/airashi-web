9e6b743f9f4a93b00cd3b12fd95bc4583835f081
nmtuan2001 <64367560+nmtuan2001@users.noreply.github.com>
2022-06-26 23:27:26 +0800

    v1.0: Set up database with Google Firebase (#3)
    * v1.0: Set up database with Google Firebase

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,13 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-
-// fake data
-import products from '../../utils/data/products';
+import { db } from '../../db/firebase';
+import { get, ref } from 'firebase/database';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req);
-
-  // fake loading time
-  setTimeout(() => {
-    res.status(200).json(products);
-  }, 800);
+  const productsRef = ref(db, 'products');
+  get(productsRef).then((snapshot) => {
+    if (snapshot.exists()) {
+      // fake loading time
+      setTimeout(() => {
+        res.status(200).json(snapshot.val());
+      }, 800);
+    } else {
+      console.log("No data available.");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
 }
